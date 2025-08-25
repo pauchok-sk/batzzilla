349068db81e55cbcd2038294635f240079c5497a
pauchok-sk <pauchoooooook@gmail.com>
2025-08-25 16:32:28 +0300

    modal size dev

--- src/js/app.js
+++ src/js/app.js
@@ -4,6 +4,7 @@ import filters from "./files/filters.js";
 import headerDrop from "./files/headerDrop.js";
 import headerScroll from "./files/headerScroll.js";
 import { headerSearch } from "./files/headerSearch.js";
+import inputmask from "./files/inputmask.js";
 import sliders from "./files/sliders.js";
 import sortSelect from "./files/sortSelect.js";
 import spoller from "./files/spoller.js";
@@ -16,5 +17,9 @@ sliders();
 headerScroll();
 sortSelect();
 filters();
+inputmask();
 
 Fancybox.bind("[data-fancybox]");
+Shareon.init();
+
+// Fancybox.show([{src: "#modal-connect", type: "inline"}])
